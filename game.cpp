942245b2aebe183caac9272a716ebc2cb8f7013c
m4gga <margriette123@gmail.com>
2021-04-09 17:58:36 +0000

    hello

--- game.cpp
+++ game.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include "snake.h"
 #include "tictactoe.h"
-#include "hangman.h"
+//#include "hangman.h"
 #include "game.h"
 
-Game::Game( Hangman hangman, TicTacToe tictactoe, Snake snake){
-    this->hangman = hangman;
+Game::Game(TicTacToe *tictactoe, Snake *snake){
+    //this->hangman = hangman;
     this->tictactoe = tictactoe;
     this->snake = snake;
 }
@@ -13,6 +13,7 @@ Game::Game( Hangman hangman, TicTacToe tictactoe, Snake snake){
 Game::Game(){}
 
 void Game::Header() {
+    cout << "\n\n\n\n\n\n\n\n";
     cout << " @@@@@@@   @@@@@@  @@@@@@@@@@  @@@@@@@@  @@@@@@ @@@@@@@  @@@@@@  @@@@@@@ " << endl;
     cout << " !@@       @@!  @@@ @@! @@! @@! @@!      !@@       @@!   @@!  @@@ @@!  @@@ " << endl;
     cout << " !@! @!@!@ @!@!@!@! @!! !!@ @!@ @!!!:!    !@@!!    @!!   @!@  !@! @!@@!@! " << endl;
@@ -21,10 +22,49 @@ void Game::Header() {
 }
 
 void Game::firstScreen(){
+    char choice;
     Header();
-    
+    cout << "\n" << "                          Ready? (y/n)                                    " << endl;
+    cout << "\n\n\n";
+
+    cin >> choice;
+    if (choice == 'y') {
+        mainScreen();
+    } 
+    else if (choice == 'n') {
+        return;
+    }
+
 }
 
 void Game::mainScreen(){
+    int choice;
+    cout << "                          -----------                                    " << endl;
+    cout << "                          Main Screen                                    " << endl;
+    cout << "                          -----------                                    " << endl;
+    cout << "                         1. Tic Tac Toe                                  " << endl;
+    cout << "                         2. Hangman                                      " << endl;
+    cout << "                         3. Snake                                        " << endl;
+    cout << "\n\n\n\n";
+    cin >> choice;
 
+    if (choice == 1) {
+        TicTacToe t;
+        t.Play();
+    } 
+    else if (choice == 2) {
+        cout << "hang";
+    }
+    else if (choice == 3) {
+        Snake s;
+        s.Play();
+    }
+
+    return;
+}
+
+int main() {
+    Game g;
+    g.firstScreen();
+    return 0;
 }
--- game.h
+++ game.h
@@ -1,13 +1,18 @@
 #include <string>
 #include "tictactoe.h"
 #include "snake.h"
-#include "hangman.h"
+//#include "hangman.h"
 using namespace std;
 
 class Game {
-public:
-    Game( Hangman hangman, TicTacToe tictactoe, Snake snake);
-    Game();
-	void mainScreen();
-    void Header();
+    public:
+        Game(TicTacToe *tictactoe, Snake *snake);
+        Game();
+        void mainScreen();
+        void Header();
+        void firstScreen();
+
+        Snake *snake;
+        //Hangman hangman;
+        TicTacToe *tictactoe;
 };
